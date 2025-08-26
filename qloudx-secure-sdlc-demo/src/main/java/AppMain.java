5bb24cf448d9d0266a9f9aa1c4c1855b6bbe4342
khedau-dxl <khedau@dxlg.com>
2025-08-26 17:14:22 +0530

    fake commit

--- qloudx-secure-sdlc-demo/src/main/java/AppMain.java
+++ qloudx-secure-sdlc-demo/src/main/java/AppMain.java
@@ -15,7 +15,7 @@ public class AppMain {
 
     public static void main(String[] args) {
         try {
-        	// ✅ Example of leaked AWS credentials (for testing purposes only)
+        	// Example of leaked AWS credentials (for testing purposasdades only)
             String awsAccessKeyId = "AKIA1234567890ABCD"; // AWS Access Key ID pattern
             String awsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"; // AWS Secret Access Key pattern
 
